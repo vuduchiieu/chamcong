433162107f962e49a0102665bfce1db3ed843766
vuduchiieu <136909425+vuduchiieu@users.noreply.github.com>
2024-06-11 17:56:47 +0700

    update 2

--- lib/core/constants/asset_path.dart
+++ lib/core/constants/asset_path.dart
@@ -7,13 +7,9 @@ class AssetPath {
   static const String introNd = '${_img}intro_nd.png';
   static const String introRd = '${_img}intro_rd.png';
   static const String logoNonText = '${_img}logo_non_text.png';
-  static const String bgrLon = '${_img}bgr_lon.png';
-  static const String threeIcRegister = '${_img}three_ic_register.png';
 
   //icon
   static const String dot = '${_icon}dot.svg';
   static const String account = '${_icon}account.svg';
   static const String company = '${_icon}company.svg';
-  static const String lighting = '${_icon}lighting.svg';
-  static const String lightingSpalash = '${_icon}lighting_splash.svg';
 }
